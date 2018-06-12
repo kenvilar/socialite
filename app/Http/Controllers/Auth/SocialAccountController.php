dfd2793910d19381b77861e31739453e168c9737
Ken Vilar <kenvilar@gmail.com>
2018-06-12 12:15:57 +0800

    Make a return statement for handleProviderCallback method

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\SocialAccount;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use Mockery\Exception;
 
@@ -14,12 +17,25 @@ class SocialAccountController extends Controller
         return Socialite::driver($provider)->redirect();
     }
 
-    public function handleProviderCallback()
+    public function handleProviderCallback($provider)
     {
         try {
-            $user = Socialite::driver('github')->user();
+            $user = Socialite::driver($provider)->user();
         } catch (Exception $exception) {
             return redirect()->route('login');
         }
+
+        $authUser = $this->findOrCreateUser($user, $provider);
+        
+        Auth::login($authUser, false);
+
+        return redirect($this->redirectTo);
+    }
+
+    public function findOrCreateUser($user, $provider)
+    {
+        $user = SocialAccount::query()->findOrNew($user->id, $provider);
+
+        return $user;
     }
 }
