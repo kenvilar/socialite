915fe8e51136f2059c44eb554a4855c42dca9596
Ken Vilar <kenvilar@gmail.com>
2018-06-17 23:57:48 +0800

    Setup google oauth socialite login

--- config/services.php
+++ config/services.php
@@ -47,4 +47,9 @@ return [
         'redirect' => env('TWITTER_REDIRECT'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_KEY'),
+        'client_secret' => env('GOOGLE_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI')
+    ],
 ];
