7d91c1f6d40d1f9bd50347b0b2a5876c262f8ef8
Ken Vilar <kenvilar@gmail.com>
2018-06-17 22:20:02 +0800

    Add env vars

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,6 +62,9 @@
                         </div>
                     </form>
                 </div>
+                <div class="panel-footer">
+
+                </div>
             </div>
         </div>
     </div>
